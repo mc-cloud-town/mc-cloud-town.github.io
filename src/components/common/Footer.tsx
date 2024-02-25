c4aef6f511eb7f38f560c08929530cd7cfba9192
whats2000 <whats2000mc@gmail.com>
2024-02-25 22:23:17 +0800

    修正: 修正尾頁樣式

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -10,7 +10,7 @@ import logo from '@/assets/logo/base.png';
 
 const TopFooterContainer = styled.div`
   background-color: #b1dde6;
-  padding: 20px 60px;
+  padding: 20px 80px;
   text-align: center;
 `;
 
