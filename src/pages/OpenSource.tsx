934fd023b363510541da325cc685edaf58782608
shane04111 <shane97213@gmail.com>
2024-02-27 02:57:21 +0800

    feat: 修改開源項目的排版及樣式

--- src/pages/OpenSource.tsx
+++ src/pages/OpenSource.tsx
@@ -1,28 +1,34 @@
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
-import { Dict } from 'styled-components/dist/types';
-import { Card } from 'antd';
 import { LeftOutlined, RightOutlined } from '@ant-design/icons';
+import { Card, Row, Col } from 'antd';
+import openSource from '@/assets/openSource/openSource.jpg';
+import languageColor from '@/assets/openSource/languageToColor.json';
 
 import HeaderImage from '#/common/HeaderImage.tsx';
 import PageHeader from '#/common/PageHeader.tsx';
 import useApi from '@/hooks/useApi';
 
 const { Meta } = Card;
-const RepoBlockList = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  gap: 30px;
-  margin-top: 20px;
-  margin-bottom: 20px;
-`;
 
 const LangShow = styled.div`
   margin-top: 10px;
 `;
 
-const CardOut = styled.div``;
+const RepoBlockList = styled.div`
+  background: #b1dde6;
+  padding: 30px;
+`;
+
+const StyledCard = styled(Card)`
+  background-color: #0d1117;
+`;
+
+type LanguageColor = {
+  [key: string]: string;
+};
+
+const Color: LanguageColor = languageColor;
 
 const OpenSourcePage = () => {
   const { t } = useTranslation();
@@ -32,35 +38,51 @@ const OpenSourcePage = () => {
   return (
     <>
       <PageHeader
-        backgroundComponent={<HeaderImage imageUrl="" />}
+        backgroundComponent={<HeaderImage imageUrl={openSource} />}
         headerTextArray={[t('opensource.title')]}
         subHeaderContentArray={[t('opensource.description')]}
       />
       <RepoBlockList>
-        {error && <p>Error</p>}
-        {loading && <p>Loading</p>}
-        {Array.isArray(data) &&
-          data
-            .filter((repo: Dict) => {
-              return !repo.name.toString().startsWith('.');
-            })
-            .map((repo: Dict) => {
-              return (
-                <CardOut
-                  key={repo.name}
-                  onClick={() => window.open(repo.html_url)}
-                >
-                  <Card hoverable style={{ width: 300 }}>
-                    <Meta title={repo.name} description={repo.description} />
+        <Row justify="center" gutter={30}>
+          {error && <p>Error</p>}
+          {loading && <p>Loading</p>}
+          {Array.isArray(data) &&
+            data
+              .filter((repo) => !repo.name.toString().startsWith('.'))
+              .map((repo) => (
+                <Col key={repo.name} xs={24} sm={12} md={8} lg={6} xl={4}>
+                  <StyledCard
+                    hoverable
+                    style={{ marginBottom: 30 }}
+                    onClick={() => window.open(repo.html_url)}
+                  >
+                    <Meta
+                      title={
+                        <span style={{ color: 'white' }}>{repo.name}</span>
+                      }
+                      description={
+                        <span style={{ color: 'white' }}>
+                          {repo.description}
+                        </span>
+                      }
+                    />
                     <LangShow>
                       <LeftOutlined />
+                      <span
+                        style={{
+                          color: Color[repo.language]
+                            ? Color[repo.language]!
+                            : 'white',
+                        }}
+                      >
+                        {repo.language ? repo.language : 'Other'}
+                      </span>
                       <RightOutlined />
-                      <span>{repo.language ? repo.language : 'Other'}</span>
                     </LangShow>
-                  </Card>
-                </CardOut>
-              );
-            })}
+                  </StyledCard>
+                </Col>
+              ))}
+        </Row>
       </RepoBlockList>
     </>
   );
