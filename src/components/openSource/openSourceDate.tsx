45806d681f5e7fb8b17916de273ca01f155b67c2
shane04111 <shane97213@gmail.com>
2024-02-27 21:26:30 +0800

    feat: 將style移出

--- src/components/openSource/openSourceDate.tsx
+++ src/components/openSource/openSourceDate.tsx
@@ -7,20 +7,25 @@ import { repoType } from '@/types/repoType';
 const { Meta } = Card;
 
 const StyledCard = styled(Card)`
-  background-color: white;
+  background-color: #f1f1f1;
+  margin-bottom: 30;
+`;
+
+const RepoName = styled.span`
+  font-weight: bold;
+`;
+
+const RepoDescription = styled.span`
+  color: #999999;
 `;
 
 const RepoCard = ({ repo }: { repo: repoType }) => {
   return (
     <Col key={repo.name} xs={24} sm={12} md={8} lg={6} xl={4}>
-      <StyledCard
-        hoverable
-        style={{ marginBottom: 30 }}
-        onClick={() => window.open(repo.html_url)}
-      >
+      <StyledCard hoverable onClick={() => window.open(repo.html_url)}>
         <Meta
-          title={<span style={{ fontWeight: 'bold' }}>{repo.name}</span>}
-          description={<span>{repo.description}</span>}
+          title={<RepoName>{repo.name}</RepoName>}
+          description={<RepoDescription>{repo.description}</RepoDescription>}
         />
       </StyledCard>
     </Col>
