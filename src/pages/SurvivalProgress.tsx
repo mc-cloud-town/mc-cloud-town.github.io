8790e4e9ca6e83e3d0f1e3151887394345d5d81b
Eric Yang <jie9481@gmail.com>
2024-02-27 16:48:03 +0800

    寫一些測試性內容在生存服進度

--- src/pages/SurvivalProgress.tsx
+++ src/pages/SurvivalProgress.tsx
@@ -1,8 +1,24 @@
+import { useTranslation } from 'react-i18next';
+
+import PageHeader from '#/common/PageHeader.tsx';
+import HeaderImage from '#/common/HeaderImage.tsx';
+
+import background from '@/assets/survivalProgress/background.jpg';
 const SurvivalProgress = () => {
+  const { t } = useTranslation();
   return (
     <>
+      <PageHeader
+        backgroundComponent={<HeaderImage imageUrl={background} />}
+        headerTextArray={[
+          t('survivalProgress.title')
+        ]}
+        subHeaderContentArray={[
+          t('survivalProgress.description'),
+        ]}
+      />
     </>
-  )
-}
+  );
+};
 
 export default SurvivalProgress;
