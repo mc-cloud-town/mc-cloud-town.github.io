f815c92b1691130905a4d421ea99b8485752e43a
whats2000 <whats2000mc@gmail.com>
2024-03-03 01:54:12 +0800

    完成: 完成開源資源頁面

--- src/pages/OpenSource.tsx
+++ src/pages/OpenSource.tsx
@@ -1,31 +1,36 @@
 import { useTranslation } from 'react-i18next';
-import { Row } from 'antd';
 import styled from 'styled-components';
 
 import PageHeader from '#/common/PageHeader.tsx';
 import HeaderImage from '#/common/HeaderImage.tsx';
-import Repo from '#/openSource/openSourceDate.tsx';
 
 import useApi from '@/hooks/useApi';
 import openSource from '@/assets/openSource/openSource.jpg';
 import { IRepoType } from '@/types/IRepoType.ts';
 import { GITHUB_API } from '@/constants';
+import { RepoCard } from 'react-repo-card-v2';
 
 const RepoBlockList = styled.div`
   background: #6f9b9c;
   padding: 30px;
 `;
+
+const FlexContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+`;
+
+const RepoCardContainer = styled.div`
+  width: 30%;
+  min-width: 280px;
+  margin: 0 auto;
+`;
+
 const OpenSourcePage = () => {
   const { t } = useTranslation();
-  const {
-    data,
-    loading,
-    error,
-  }: {
-    data: IRepoType[] | null;
-    loading: boolean;
-    error: { message: string } | null;
-  } = useApi(GITHUB_API);
+  const { data, loading, error } = useApi<IRepoType[]>(GITHUB_API);
 
   return (
     <>
@@ -35,17 +40,15 @@ const OpenSourcePage = () => {
         subHeaderContentArray={[t('opensource.description')]}
       />
       <RepoBlockList>
-        <Row justify="center" gutter={30}>
-          {error && <p>Error</p>}
-          {loading && <p>Loading</p>}
-          {Array.isArray(data) &&
-            data.map(
-              (repo) =>
-                !repo.name.startsWith('.') && (
-                  <Repo key={repo.name} repo={repo} />
-                ),
-            )}
-        </Row>
+        <FlexContainer>
+          {error && <p>{t('error')}</p>}
+          {loading && <p>{t('loading')}</p>}
+          {Array.isArray(data) && data.map((repo) => !repo.name.startsWith('.') && (
+            <RepoCardContainer key={repo.id}>
+              <RepoCard repository={repo} showIssues={false} />
+            </RepoCardContainer>
+          ))}
+        </FlexContainer>
       </RepoBlockList>
     </>
   );
