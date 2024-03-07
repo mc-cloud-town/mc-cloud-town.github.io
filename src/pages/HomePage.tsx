afab5b1abe3bbcf932e4e99b021aadcc2928abcd
whats2000 <whats2000mc@gmail.com>
2024-03-08 02:15:29 +0800

    修改: 結構化首頁json

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,46 +9,8 @@ import CarouselSection from '#/common/CarouselSection.tsx';
 import HeaderBackground from '#/homePage/header/HeaderBackground.tsx';
 import HeaderTimer from '#/homePage/header/HeaderTimer.tsx';
 
-import { IImageContent } from '@/types/IImageContent.ts';
-
-interface IHomePageCard {
-  imageUrl: string;
-  title: string;
-  description: string;
-  button: string;
-  buttonLink: string;
-}
-
 const HomePage = () => {
   const { t } = useTranslation();
-  const features: string[] = t('home.about.features', { returnObjects: true });
-  const aboutSectionImageContent: IImageContent = {
-    imageUrl: t('home.about.imageUrl'),
-    title: t('home.about.title'),
-    subTitle: t('home.about.subTitle'),
-    features: features,
-    buttons: [
-      {
-        text: t('home.about.button'),
-        link: '/survival',
-      },
-    ],
-  };
-
-  const cards: IHomePageCard[] = t('home.feature.card', { returnObjects: true });
-  const featureSectionImageContents: IImageContent[] = cards.map((card) => {
-    return {
-      imageUrl: card.imageUrl,
-      title: card.title,
-      paragraph: card.description,
-      buttons: [
-        {
-          text: card.button,
-          link: `/${card.buttonLink}`,
-        },
-      ],
-    };
-  });
 
   return (
     <>
@@ -71,19 +33,19 @@ const HomePage = () => {
         useTyped={true}
       />
       <ImageContentSection
-        imageContent={aboutSectionImageContent}
+        imageContent={t('home.about', { returnObjects: true })}
       />
       <CardsSection
         title={t('home.feature.title')}
         darkMode={true}
-        imageContentSections={featureSectionImageContents}
+        imageContentSections={t('home.feature.card', { returnObjects: true })}
       />
       <CarouselSection
         title={t('home.carousel.title')}
         imageContentsSections={[
-          featureSectionImageContents,
-          featureSectionImageContents,
-          featureSectionImageContents,
+          t('home.feature.card', { returnObjects: true }),
+          t('home.feature.card', { returnObjects: true }),
+          t('home.feature.card', { returnObjects: true }),
         ]}
       />
     </>
