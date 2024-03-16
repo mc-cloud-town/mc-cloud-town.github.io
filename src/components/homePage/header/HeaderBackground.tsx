b9db85077058f4a95bba4a8c4449255098dbdb21
whats2000 <whats2000mc@gmail.com>
2024-03-17 03:11:03 +0800

    新增: 新增影片載入placeholder

--- src/components/homePage/header/HeaderBackground.tsx
+++ src/components/homePage/header/HeaderBackground.tsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import YouTubeIframeLoader from 'youtube-iframe';
 
-const BackgroundVideo = styled.iframe`
+const BackgroundContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+`;
+
+const VideoPlaceholder = styled.img<{ $isVideoReady: boolean }>`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  transition: opacity 1s ease-out;
+  opacity: ${({ $isVideoReady }) => ($isVideoReady ? 0 : 1)};
+  z-index: -1;
+`;
+
+const BackgroundVideo = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
@@ -9,23 +27,52 @@ const BackgroundVideo = styled.iframe`
   min-width: 100vw;
   min-height: calc(120vw * 9 / 16);
   transform: translate(-50%, -50%);
-  z-index: 1;
+  z-index: -1;
   overflow: hidden;
-  border: 0;
+
+  iframe {
+    width: 100%;
+    height: 100%;
+    border: 0;
+  }
 `;
 
 const HeaderBackground = () => {
-  const youtubeId = 'USJWCs-rlc0';
-  const start = 9;
-  const videoSrc = `https://www.youtube.com/embed/${youtubeId}?controls=0&disablekb=1&fs=0&iv_load_policy=3&start=${start}&autoplay=1&mute=1&playsinline=1&loop=1&playlist=${youtubeId}&frameborder=0`;
+  const [isVideoReady, setIsVideoReady] = useState(false);
+  const youtubeId = 'USJWCs-rlc0'; // Your YouTube Video ID here
+  const placeholderUrl = `https://img.youtube.com/vi/${youtubeId}/maxresdefault.jpg`;
+
+  useEffect(() => {
+    YouTubeIframeLoader.load((YT) => {
+      new YT.Player('youtube-background', {
+        videoId: youtubeId,
+        events: {
+          onReady: () => setTimeout(() => setIsVideoReady(true), 1000),
+        },
+        playerVars: {
+          autoplay: 1,
+          controls: 0,
+          mute: 1,
+          loop: 1,
+          playlist: youtubeId,
+          playsinline: 1,
+          fs: 0,
+          iv_load_policy: 3,
+          modestbranding: 1,
+          rel: 0,
+          showinfo: 0,
+          start: 9,
+        },
+      });
+    });
+  }, [youtubeId]);
+
   return (
-    <BackgroundVideo
-      src={videoSrc}
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      title="YouTube Video Background"
-    />
+    <BackgroundContainer>
+      <VideoPlaceholder src={placeholderUrl} alt="Background video placeholder" $isVideoReady={isVideoReady} />
+      <BackgroundVideo id="youtube-background" />
+    </BackgroundContainer>
   );
-}
+};
 
 export default HeaderBackground;
