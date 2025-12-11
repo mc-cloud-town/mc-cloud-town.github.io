c5794519cf7ac4a7bc19c9d1aa23f932f316f204
whats2000 <whats2000mc@gmail.com>
2025-12-11 16:15:14 +0800

    Fix: Fix the resolve image in Vite 5

--- src/utils/getImageUrl.ts
+++ src/utils/getImageUrl.ts
@@ -1,9 +1,14 @@
+const images = import.meta.glob('/src/assets/**/*', {
+  eager: true,
+}) as Record<string, { default: string }>;
+
 /**
  * Get the URL of an image in the assets folder
  * @param snippet {string} - The snippet of the image URL
  */
 const getImageUrl = (snippet: string) => {
-  return new URL(`/src/assets/${snippet}`, import.meta.url).href;
+  const key = `/src/assets/${snippet}`;
+  return images[key]?.default ?? '';
 };
 
 export default getImageUrl;
